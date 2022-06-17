b575b28d271fd9d66ca5915dbc44bd840d9297f1
DiegoAugustoDev <98817641+DiegoAugustoDev@users.noreply.github.com>
2022-06-17 14:19:29 -0300

    Atualização do software

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
-
-function colorChange(i){
-    let red = Math.floor(Math.random()*255)
-let green = Math.floor(Math.random()*255)
-let blue = Math.floor(Math.random()*255)
-
+const colorArea = document.getElementById("colorArea")
 const rgb = document.getElementById("rgb")
-rgb.innerText = `rgb(${red}, ${green}, ${blue})`
 
-document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+function colorChange(){
+    colorArea.style.display = "flex"
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+
+    rgb.innerHTML = `RGB(${red}, ${green}, ${blue})`
+
+    document.body.style.backgroundColor = `RGB(${red}, ${green}, ${blue})`
 }
